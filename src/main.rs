ab24bc15fe1bcc4b6fc9069016c89653a079cf01
James T <tharit.rt@gmail.com>
2024-06-23 12:53:55 +0700

    Format code

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,16 @@
-use std::io::{self, Write};
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io::{self, Write};
 
 fn main() {
     println!("Guessing the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    
-    // println!("The secret number is: {secret_number}");
 
     loop {
+        let guess = receive_input();
 
-        let guess = receive_input(); 
-    
-        let guess: u32 = match guess.trim().parse() {
+        let guess: u16 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
@@ -26,25 +23,23 @@ fn main() {
             Ordering::Equal => {
                 println!("you win!");
                 break;
-            } 
+            }
         }
     }
 }
 
 fn receive_input() -> String {
-   
     print!("Input: ");
     io::stdout().flush().expect("flush failed!");
-    
+
     // variables are immutable by default, add mut to make a variable mutable
     let mut guess = String::new();
 
     io::stdin()
-    // & indicates that this argument is reference
-    // references are immutable by default, add &mut to make it mutable
+        // & indicates that this argument is reference
+        // references are immutable by default, add &mut to make it mutable
         .read_line(&mut guess)
         .expect("failed to read line");
 
     return guess;
 }
-
